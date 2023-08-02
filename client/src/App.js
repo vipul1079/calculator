52dc0d87380481aee64805879bb0b58da6811fa5
vipul1079 <vipulsharma1079@gmail.com>
2023-08-02 20:42:09 +0530

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Calculator
+      </header>
+    </div>
+  );
+}
+
+export default App;
